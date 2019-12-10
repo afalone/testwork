16c16d898318bffe463a434a43a19fdf75e6b2c4
afa <OZavgorodniy@zaoeps.local>
2019-12-10 12:18:24 +0300

    ready

--- app.rb
+++ app.rb
@@ -1,14 +1,15 @@
 require 'sinatra'
+require 'sinatra/json'
 require './tasks'
 class App < Sinatra::Base
   get '/tasks' do
     json Task.list
   end
 
-  get '/tasks' do
+  post '/tasks' do
     prio = params[prio].to_i
-    prio = 0 unless prio.in?(0..4)
-    result = Task.new(complexity: params[:complexity], prio: prio).shoot
-    render json: { success: result }
+    prio = 0 unless (0..4).include?(prio)
+    result = Task.new(complexity: params[:complexity].to_i, prio: prio).shoot
+    json({ success: result })
   end
 end
